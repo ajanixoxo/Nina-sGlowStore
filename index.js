3ee28b5dee0f024798dc0df4a73f2925d051dfab
Ade <joelayomide35@gmail.com>
2025-03-08 01:11:53 +0100

    Testing again

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const bcrypt = require('bcrypt');
 // app.use(cors())
 app.use(flash());
 
+app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
