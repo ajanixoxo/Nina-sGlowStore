93419d39367e6efb30c45042917a90443dc1c065
Ade <joelayomide35@gmail.com>
2025-03-08 01:24:51 +0100

    Update again

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(flash());
 app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "public")))
 
 
 
